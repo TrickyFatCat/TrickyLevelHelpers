0e03483a06ad49837a42d5142af96e9fc53c0a4c
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-02-15 23:15:27 +0200

    Fix bugs

--- Source/TrickyLevelHelpers/Private/ActorOrganizerSpline.cpp
+++ Source/TrickyLevelHelpers/Private/ActorOrganizerSpline.cpp
@@ -17,22 +17,30 @@ void AActorOrganizerSpline::CreateActors()
 {
 #if WITH_EDITORONLY_DATA
 
-	if (!ActorClass || ActorsAmount <= 0)
+	const int32 PointsAmount = bUseCustomSpacing
+		                           ? FMath::Floor(SplineComponent->GetSplineLength() / Spacing)
+		                           : ActorsAmount;
+	
+	if (!ActorClass || PointsAmount <= 0)
 	{
 		return;
 	}
 
 	TArray<FVector> Locations;
-	const int32 PointsAmount = bUseCustomSpacing
-		                           ? FMath::Floor(SplineComponent->GetSplineLength() / Spacing)
-		                           : ActorsAmount;
 	ULevelHelpersLibrary::CalculateSplineLocations(SplineComponent, Locations, PointsAmount, LocationOffset);
 
 	if (Actors.Num() == PointsAmount)
 	{
 		for (int32 i = 0; i < PointsAmount; ++i)
 		{
-			Actors[i]->SetActorRelativeLocation(Locations[i]);
+			AActor* Actor = Actors[i];
+
+			if (!IsValid(Actor))
+			{
+				continue;
+			}
+			
+			Actor->SetActorRelativeLocation(Locations[i]);
 		}
 
 		return;
