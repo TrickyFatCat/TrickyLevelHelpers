0f9d4840dc4c5b91323e2fef9ab49b87cbc3ac58
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-02-17 22:04:07 +0200

    Change include

--- Source/TrickyLevelHelpers/Private/ActorsGeneratorBase.cpp
+++ Source/TrickyLevelHelpers/Private/ActorsGeneratorBase.cpp
@@ -2,6 +2,7 @@
 
 
 #include "ActorsGeneratorBase.h"
+#include "Engine/World.h"
 
 AActorsGeneratorBase::AActorsGeneratorBase()
 {
--- Source/TrickyLevelHelpers/Public/ActorsGeneratorBase.h
+++ Source/TrickyLevelHelpers/Public/ActorsGeneratorBase.h
@@ -5,7 +5,6 @@
 #include "CoreMinimal.h"
 #include "LevelHelpersLibrary.h"
 #include "GameFramework/Actor.h"
-#include "Engine/World.h"
 #include "ActorsGeneratorBase.generated.h"
 
 UCLASS(Abstract, NotBlueprintable)
