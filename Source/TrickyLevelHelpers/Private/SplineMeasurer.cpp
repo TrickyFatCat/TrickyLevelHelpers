f64ddf2d67602c48a071b116b9f3739b3a9a4de2
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-14 22:45:59 +0300

    Create SplineMeasurer actor

---
+++ Source/TrickyLevelHelpers/Private/SplineMeasurer.cpp
@@ -0,0 +1,101 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "SplineMeasurer.h"
+
+#include "Components/SplineComponent.h"
+#include "Components/TextRenderComponent.h"
+
+using TextObjPtr = TObjectPtr<UTextRenderComponent>;
+
+ASplineMeasurer::ASplineMeasurer()
+{
+#if WITH_EDITORONLY_DATA
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+#else
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+#endif
+
+	bIsEditorOnlyActor = true;
+
+#if WITH_EDITORONLY_DATA
+
+	SplineComponent = CreateDefaultSubobject<USplineComponent>("Spline");
+	SetRootComponent(ToRawPtr(SplineComponent));
+	SplineComponent->EditorUnselectedSplineSegmentColor = FLinearColor::Red;
+	SplineComponent->EditorSelectedSplineSegmentColor = FLinearColor::Yellow;
+	SplineComponent->EditorTangentColor = FLinearColor::Green;
+
+	auto CreateText = [&](TextObjPtr& TextRender, const FName& Name) -> void
+	{
+		TextRender = CreateDefaultSubobject<UTextRenderComponent>(Name);
+		TextRender->TextRenderColor = FColor::Magenta;
+		TextRender->SetupAttachment(GetRootComponent());
+	};
+
+	CreateText(MeasurementText, "EndText");
+	CreateText(CustomMeasurementText, "CustomText");
+	CustomMeasurementText->SetVisibility(bShowCustomMeasurementText);
+
+#endif
+}
+
+bool ASplineMeasurer::ShouldTickIfViewportsOnly() const
+{
+	return true;
+}
+
+void ASplineMeasurer::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+#if WITH_EDITORONLY_DATA
+
+	const int32 LastSplinePoint = SplineComponent->GetNumberOfSplinePoints();
+	const int32 LastPointIndex = SplineComponent->IsClosedLoop() ? LastSplinePoint : LastSplinePoint - 1;
+
+	auto UpdateText = [&](const TextObjPtr& TextComponent, const int32 PointIndex)-> void
+	{
+		TextComponent->SetRelativeLocation(
+			SplineComponent->GetLocationAtSplinePoint(PointIndex, ESplineCoordinateSpace::Local));
+		const float Distance = SplineComponent->GetDistanceAlongSplineAtSplinePoint(PointIndex);
+		const FString InfoText = FString::Printf(
+			TEXT("Units: %d\nMeters: %.2f"), static_cast<int32>(Distance), Distance / 100.f);
+		TextComponent->SetText(FText::FromString(InfoText));
+	};
+
+	UpdateText(MeasurementText, LastPointIndex);
+
+	CustomPointIndex = CustomPointIndex > LastPointIndex ? LastPointIndex - 1 : CustomPointIndex;
+	UpdateText(CustomMeasurementText, CustomPointIndex);
+	CustomMeasurementText->SetVisibility(bShowCustomMeasurementText);
+	
+#endif
+}
+
+void ASplineMeasurer::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void ASplineMeasurer::Tick(float DeltaSeconds)
+{
+#if WITH_EDITORONLY_DATA
+	const int32 LastSplinePoint = SplineComponent->GetNumberOfSplinePoints();
+	const int32 LastPointIndex = SplineComponent->IsClosedLoop() ? LastSplinePoint : LastSplinePoint - 1;
+
+	DrawDebugSphere(GetWorld(),
+	                SplineComponent->GetLocationAtSplinePoint(LastPointIndex, ESplineCoordinateSpace::World),
+	                64.f,
+	                32,
+	                FColor::Magenta,
+	                false,
+	                -1,
+	                0,
+	                5.f);
+#endif
+
+	Super::Tick(DeltaSeconds);
+}
---
+++ Source/TrickyLevelHelpers/Public/SplineMeasurer.h
@@ -0,0 +1,43 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SplineMeasurer.generated.h"
+
+class USplineComponent;
+class UTextRenderComponent;
+
+UCLASS(Blueprintable)
+class TRICKYLEVELHELPERS_API ASplineMeasurer : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASplineMeasurer();
+
+protected:
+	virtual bool ShouldTickIfViewportsOnly() const override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<USplineComponent> SplineComponent = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Component")
+	TObjectPtr<UTextRenderComponent> MeasurementText = nullptr;
+
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<UTextRenderComponent> CustomMeasurementText = nullptr;
+
+	UPROPERTY(EditAnywhere)
+	bool bShowCustomMeasurementText = false;
+
+	UPROPERTY(EditAnywhere, meta=(EditCondition="bShowCustomMeasurementText", ClampMin = "0"))
+	int32 CustomPointIndex = 1;
+};
