ef1d008c803520c36606e3373ef21373c8461ccb
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-16 00:58:09 +0300

    Create debug text component to show debug text information in editor

---
+++ Source/TrickyLevelHelpers/Private/DebugTextComponent.cpp
@@ -0,0 +1,121 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "DebugTextComponent.h"
+
+#include "Components/SplineComponent.h"
+#include "Engine/Canvas.h"
+
+FDebugSceneProxy::FDebugSceneProxy(const UPrimitiveComponent* InComponent,
+                                   FDebugSceneProxyData* ProxyData) : FDebugRenderSceneProxy(InComponent)
+{
+	DrawType = EDrawType::SolidAndWireMeshes;
+	ViewFlagName = "Editor";
+
+	this->ProxyData = *ProxyData;
+
+	for (const auto& Text : ProxyData->DebugLabels)
+	{
+		this->Texts.Add({
+			Text.Text,
+			Text.Location,
+			FColor::White
+		});
+	}
+}
+
+void FDebugTextDelegateHelper::DrawDebugLabels(UCanvas* Canvas, APlayerController* PlayerController)
+{
+	if (!Canvas)
+	{
+		return;
+	}
+
+	const FColor OldDrawColor = Canvas->DrawColor;
+	Canvas->SetDrawColor(FColor::White);
+	const FSceneView* View = Canvas->SceneView;
+	const UFont* Font = GEngine->GetSmallFont();
+	const FDebugSceneProxyData::FDebugText* DebugText = DebugLabels.GetData();
+
+	for (int32 i = 0; i < DebugLabels.Num(); ++i, ++DebugText)
+	{
+		if (View->ViewFrustum.IntersectSphere(DebugText->Location, 1.0f))
+		{
+			const FVector ScreenLoc = Canvas->Project(DebugText->Location);
+			Canvas->DrawText(Font, DebugText->Text, ScreenLoc.X, ScreenLoc.Y);
+		}
+	}
+
+	Canvas->SetDrawColor(OldDrawColor);
+}
+
+void FDebugTextDelegateHelper::SetupFromProxy(const FDebugSceneProxy* InSceneProxy)
+{
+	if (bResetLabels)
+	{
+		DebugLabels.Reset();
+	}
+
+	DebugLabels.Append(InSceneProxy->ProxyData.DebugLabels);
+}
+
+UDebugTextComponent::UDebugTextComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+FDebugRenderSceneProxy* UDebugTextComponent::CreateDebugSceneProxy()
+{
+	if (DebugLabels.Num() == 0)
+	{
+		return nullptr;
+	}
+	// USplineComponent* SplineComponent = GetOwner()->FindComponentByClass<USplineComponent>();
+
+	// if (!SplineComponent)
+	// {
+	// return nullptr;
+	// }
+
+	// const int32 PointsNum = SplineComponent->GetNumberOfSplinePoints();
+	// const int32 LastPointIndex = SplineComponent->IsClosedLoop() ? PointsNum : PointsNum - 1;
+
+	FDebugSceneProxyData ProxyData;
+
+	// for (int32 i = 0; i <= LastPointIndex; ++i)
+	// {
+	// const FVector Location = SplineComponent->GetLocationAtSplinePoint(
+	// i, ESplineCoordinateSpace::World);
+	// const float Distance = SplineComponent->GetDistanceAlongSplineAtSplinePoint(i);
+	// const FString InfoText = FString::Printf(
+	// TEXT("Units: %d\nMeters: %.2f"), static_cast<int32>(Distance), Distance / 100.f);
+	// ProxyData.DebugLabels.Add({Location, InfoText});
+	// }
+
+	for (TPair<FString, FVector>& Label : DebugLabels)
+	{
+		ProxyData.DebugLabels.Add({Label.Value, Label.Key});
+	}
+
+	FDebugSceneProxy* DebugSceneProxy = new FDebugSceneProxy(this, &ProxyData);
+
+	if (DebugSceneProxy)
+	{
+		DebugDrawDelegateManager.SetupFromProxy(DebugSceneProxy);
+	}
+
+	return DebugSceneProxy;
+}
+
+FBoxSphereBounds UDebugTextComponent::CalcBounds(const FTransform& LocalToWorld) const
+{
+	return FBoxSphereBounds(FBox(FVector(-1000, -1000, -1000), FVector(1000, 1000, 1000)));
+}
+
+void UDebugTextComponent::SetDebugText(const TMap<FString, FVector>& LabelsData, const bool bResetLabels)
+{
+	DebugLabels.Empty();
+	DebugLabels = LabelsData;
+	this->MarkRenderStateDirty();
+	DebugDrawDelegateManager.bResetLabels = bResetLabels;
+}
---
+++ Source/TrickyLevelHelpers/Public/DebugTextComponent.h
@@ -0,0 +1,73 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Debug/DebugDrawComponent.h"
+#include "DebugTextComponent.generated.h"
+
+struct FDebugSceneProxyData
+{
+	struct FDebugText
+	{
+		FVector Location;
+		FString Text;
+
+		FDebugText() {}
+		FDebugText(const FVector& InLocation, const FString& InText) : Location(InLocation), Text(InText){}
+	};
+	
+	TArray<FDebugText> DebugLabels;
+};
+
+class FDebugSceneProxy : public FDebugRenderSceneProxy
+{
+public:
+	FDebugSceneProxy(const UPrimitiveComponent* InComponent, FDebugSceneProxyData* ProxyData);
+
+	FDebugSceneProxyData ProxyData;
+};
+
+class FDebugTextDelegateHelper : public FDebugDrawDelegateHelper
+{
+public:
+	virtual void DrawDebugLabels(UCanvas* Canvas, APlayerController*) override;
+
+	void SetupFromProxy(const FDebugSceneProxy* InSceneProxy);
+
+	bool bResetLabels = false;
+
+	TArray<FDebugSceneProxyData::FDebugText> DebugLabels;
+};
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRICKYLEVELHELPERS_API UDebugTextComponent : public UDebugDrawComponent
+{
+	GENERATED_BODY()
+
+public:
+	UDebugTextComponent();
+
+protected:
+	FDebugTextDelegateHelper DebugDrawDelegateManager;
+
+	UPROPERTY(EditAnywhere)
+	FString DebugText = "Debug Text";
+	
+	UPROPERTY(EditAnywhere)
+	FVector TextLocation{FVector::ZeroVector};
+
+	UPROPERTY(EditAnywhere)
+	TMap<FString, FVector> DebugLabels;
+
+	virtual FDebugRenderSceneProxy* CreateDebugSceneProxy() override;
+	
+	virtual FDebugDrawDelegateHelper& GetDebugDrawDelegateHelper() override { return DebugDrawDelegateManager; }
+	
+	virtual  FBoxSphereBounds CalcBounds(const FTransform& LocalToWorld) const override;
+
+public:
+	UFUNCTION(BlueprintCallable)
+	void SetDebugText(const TMap<FString, FVector>& LabelsData, const bool bResetLabels);
+};
+
