dcff34f54b310615881b2437b23a27655f06d595
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-17 00:07:19 +0300

    Create simple spline actor class

---
+++ Source/TrickyLevelHelpers/Private/SplineActor.cpp
@@ -0,0 +1,29 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "SplineActor.h"
+
+#include "Components/DebugTextComponent.h"
+#include "Components/SplineComponent.h"
+
+ASplineActor::ASplineActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	SplineComponent = CreateDefaultSubobject<USplineComponent>("Spline");
+	SetRootComponent(ToRawPtr(SplineComponent));
+
+#if WITH_EDITORONLY_DATA
+	
+	auto CreateDebugText = [&](TObjectPtr<UDebugTextComponent>& DebugText, const FName& Name) -> void
+	{
+		DebugText = CreateDefaultSubobject<UDebugTextComponent>(Name);
+		DebugText->SetupAttachment(GetRootComponent());
+		DebugText->SetDrawOneLabel(false);
+	};
+
+	CreateDebugText(DistanceDebug, "DistanceDebug");
+	CreateDebugText(SectorsDebug, "SectorsDebug");
+
+#endif
+}
---
+++ Source/TrickyLevelHelpers/Public/SplineActor.h
@@ -0,0 +1,67 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SplineActor.generated.h"
+
+class USplineComponent;
+class UDebugTextComponent;
+
+UCLASS(Blueprintable, BlueprintType)
+class TRICKYLEVELHELPERS_API ASplineActor : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ASplineActor();
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, Category="Components")
+	TObjectPtr<USplineComponent> SplineComponent = nullptr;
+
+private:
+#if WITH_EDITORONLY_DATA
+	
+	UPROPERTY()
+	TObjectPtr<UDebugTextComponent> DistanceDebug = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UDebugTextComponent> SectorsDebug = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="SplineActor", AdvancedDisplay)
+	bool bShowDebugInGame = false;
+
+	UPROPERTY(EditAnywhere, Category="SplineActor", AdvancedDisplay)
+	bool bShowDistanceDebug = false;
+
+	UPROPERTY(EditAnywhere,
+		Category="SplineActor",
+		AdvancedDisplay,
+		meta=(EditCondition="bShowDistanceDebug", EditConditionHides))
+	FLinearColor DistanceDebugColor{FColor::Magenta};
+
+	UPROPERTY(EditAnywhere,
+		Category="SplineActor",
+		AdvancedDisplay,
+		meta=(EditCondition="bShowDistanceDebug", EditConditionHides))
+	float DistanceDebugScale = 1.0f;
+
+	UPROPERTY(EditAnywhere, Category="SplineActor", AdvancedDisplay)
+	bool bShowSectorsDebug = false;
+	
+	UPROPERTY(EditAnywhere,
+		Category="SplineActor",
+		AdvancedDisplay,
+		meta=(EditCondition="bShowSectorsDebug", EditConditionHides))
+	FLinearColor SectorsDebugColor{FColor::Cyan};
+
+	UPROPERTY(EditAnywhere,
+		Category="SplineActor",
+		AdvancedDisplay,
+		meta=(EditCondition="bShowSectorsDebug", EditConditionHides))
+	float SectorsDebugScale = 1.0f;
+
+#endif
+};
