4ba7e27449af0ced3f63ebbf80cad3e413503c6e
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-17 23:31:44 +0300

    Create LineRuler actor

---
+++ Source/TrickyLevelHelpers/Private/Rulers/LineRuler.cpp
@@ -0,0 +1,116 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Rulers/LineRuler.h"
+
+#include "Components/BillboardComponent.h"
+
+
+ALineRuler::ALineRuler()
+{
+	Root = CreateDefaultSubobject<UBillboardComponent>("Root");
+	SetRootComponent(ToRawPtr(Root));
+
+#if WITH_EDITORONLY_DATA
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinder<UTexture2D> SpriteTexture;
+		FName ID_Misc;
+		FText NAME_Misc;
+
+		FConstructorStatics()
+			: SpriteTexture(TEXT("/Engine/EditorResources/S_Note"))
+			  , ID_Misc(TEXT("Misc"))
+			  , NAME_Misc(NSLOCTEXT("SpriteCategory", "Misc", "Misc"))
+		{
+		}
+	};
+
+	static FConstructorStatics ConstructorStatics;
+	Root->SetSprite(ConstructorStatics.SpriteTexture.Object);
+#else
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+#endif
+
+	bIsEditorOnlyActor = true;
+}
+
+bool ALineRuler::ShouldTickIfViewportsOnly() const
+{
+	return true;
+}
+
+void ALineRuler::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+#if WITH_EDITORONLY_DATA
+
+#endif
+}
+
+void ALineRuler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+#if WITH_EDITORONLY_DATA
+	DrawLine(Distance.X, GetActorForwardVector(), FColor::Red);
+	DrawLine(Distance.Y, GetActorRightVector(), FColor::Green);
+	DrawLine(Distance.Z, GetActorUpVector(), FColor::Blue);
+
+	if (bDrawMarks)
+	{
+		DrawMarks(Distance.X, MarksDistance.X, GetActorForwardVector(), FColor::Red);
+		DrawMarks(Distance.Y, MarksDistance.Y, GetActorRightVector(), FColor::Green);
+		DrawMarks(Distance.Z, MarksDistance.Z, GetActorUpVector(), FColor::Blue);
+	}
+#endif
+}
+
+void ALineRuler::DrawLine(const double Length, const FVector& Axis, const FColor& Color) const
+{
+	if (Length == 0.f)
+	{
+		return;
+	}
+
+	const FVector LineStart = GetActorLocation();
+	const FVector LineEnd = LineStart + Axis * Length;
+
+	DrawDebugLine(GetWorld(),
+	              LineStart,
+	              LineEnd,
+	              Color,
+	              false,
+	              -1,
+	              0,
+	              LineThickness);
+}
+
+void ALineRuler::DrawMarks(const double Length,
+                           const double MarkDistance,
+                           const FVector& Axis,
+                           const FColor& Color) const
+{
+	if (Length == 0.0 || MarkDistance == 0.0)
+	{
+		return;
+	}
+
+	const int32 Amount = FMath::FloorToInt32(FMath::Abs(Length) / MarkDistance);
+
+
+	for (int32 i = 0; i < Amount; ++i)
+	{
+		const FVector Location = GetActorLocation() + Axis * MarkDistance * (i + 1) * FMath::Sign(Length);
+		DrawDebugCrosshairs(GetWorld(),
+		                    Location,
+		                    GetActorRotation(),
+		                    MarksScale,
+		                    Color);
+	}
+}
---
+++ Source/TrickyLevelHelpers/Public/Rulers/LineRuler.h
@@ -0,0 +1,48 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "LineRuler.generated.h"
+
+class UBillboardComponent;
+
+UCLASS()
+class TRICKYLEVELHELPERS_API ALineRuler : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ALineRuler();
+
+protected:
+	virtual bool ShouldTickIfViewportsOnly() const override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UBillboardComponent> Root = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="LineRuler")
+	FVector Distance{100.0};
+
+	UPROPERTY(EditAnywhere, Category="LineRuler")
+	float LineThickness = 2.f;
+
+	UPROPERTY(EditAnywhere, Category="LineRuler")
+	bool bDrawMarks = false;
+
+	UPROPERTY(EditAnywhere, Category="LineRuler", meta=(EditCondition="bDrawMarks"))
+	FVector MarksDistance{100.0};
+
+	UPROPERTY(EditAnywhere, Category="LineRuler", meta=(EditCondition="bDrawMarks"))
+	float MarksScale = 50.f;
+
+	void DrawLine(const double Length, const FVector& Axis, const FColor& Color) const;
+	
+	void DrawMarks(const double Length, double MarkDistance, const FVector& Axis, const FColor& Color) const;
+};
