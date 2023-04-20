2ab86e005be3f9a0e23243ea4b6e7462d3d5c37b
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-20 23:51:12 +0300

    Create a RadiusRuler class

---
+++ Source/TrickyLevelHelpers/Private/Rulers/RadiusRuler.cpp
@@ -0,0 +1,135 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Rulers/RadiusRuler.h"
+
+#include "Components/BillboardComponent.h"
+#include "Components/DebugTextComponent.h"
+
+
+ARadiusRuler::ARadiusRuler()
+{
+	Root = CreateDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(Root);
+
+#if WITH_EDITORONLY_DATA
+	Billboard = CreateEditorOnlyDefaultSubobject<UBillboardComponent>("Billboard");
+	Billboard->SetupAttachment(GetRootComponent());
+
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = true;
+
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinder<UTexture2D> SpriteTexture;
+		FName ID_Misc;
+		FText NAME_Misc;
+
+		FConstructorStatics()
+			: SpriteTexture(TEXT("/Engine/EditorResources/S_Note"))
+			  , ID_Misc(TEXT("Misc"))
+			  , NAME_Misc(NSLOCTEXT("SpriteCategory", "Misc", "Misc"))
+		{
+		}
+	};
+
+	static FConstructorStatics ConstructorStatics;
+	Billboard->SetSprite(ConstructorStatics.SpriteTexture.Object);
+	Billboard->bUseInEditorScaling = false;
+	Billboard->bIsScreenSizeScaled = true;
+	Billboard->ScreenSize = 0.001;
+
+	DebugText = CreateEditorOnlyDefaultSubobject<UDebugTextComponent>("Name");
+	DebugText->SetupAttachment(GetRootComponent());
+	DebugText->SetDrawInGame(false);
+	DebugText->SetDrawOneLabel(true);
+#else
+	PrimaryActorTick.bCanEverTick = false;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+#endif
+
+	bIsEditorOnlyActor = true;
+}
+
+bool ARadiusRuler::ShouldTickIfViewportsOnly() const
+{
+	return true;
+}
+
+void ARadiusRuler::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+#if WITH_EDITORONLY_DATA
+	bIsEditorOnlyActor = !bShowInGame;
+	DebugText->SetDrawInGame(bShowInGame);
+
+	FDebugLabelData DebugLabelData;
+	DebugLabelData.Color = Color;
+	DebugLabelData.TextScale = 1.15;
+	DebugLabelData.bUseCustomLocation = true;
+
+	const float Theta = FMath::DegreesToRadians(TextAngleOffset);
+	const FVector Centre = GetActorLocation();
+	FVector Location = Centre;
+	Location.X = Radius * FMath::Cos(Theta) + Centre.X;
+	Location.Y = Radius * FMath::Sin(Theta) + Centre.Y;
+	DebugLabelData.Location = Location;
+	
+	const FString HeaderText = FString::Printf(TEXT("%s\n---------\n"), *NoteText);
+	const FString LengthData = FString::Printf(
+		TEXT("%sUnits: %d\nMeters: %.2f"), *HeaderText,
+		static_cast<int32>(Radius),
+		Radius / 100.f);
+	DebugLabelData.Text = LengthData;
+	DebugText->SetDebugLabel(DebugLabelData);
+#endif
+}
+
+
+void ARadiusRuler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+#if WITH_EDITORONLY_DATA
+	switch (Shape)
+	{
+	case ERadiusRulerShape::Circle:
+		DrawCircle(GetWorld(),
+		           GetActorLocation(),
+		           GetActorForwardVector(),
+		           GetActorRightVector(),
+		           Color,
+		           Radius,
+		           32,
+		           false,
+		           -1,
+		           0,
+		           3.f);
+		break;
+
+	case ERadiusRulerShape::Sphere:
+		DrawDebugSphere(GetWorld(),
+		                GetActorLocation(),
+		                Radius,
+		                24,
+		                Color,
+		                false,
+		                -1,
+		                0,
+		                3.f);
+		break;
+
+	case ERadiusRulerShape::Cylinder:
+		DrawDebugCylinder(GetWorld(),
+		                  GetActorLocation(),
+		                  GetActorLocation() + GetActorUpVector() * Height,
+		                  Radius,
+		                  24,
+		                  Color,
+		                  false,
+		                  -1,
+		                  0,
+		                  3.f);
+		break;
+	}
+#endif
+}
---
+++ Source/TrickyLevelHelpers/Public/Rulers/RadiusRuler.h
@@ -0,0 +1,68 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RadiusRuler.generated.h"
+
+class USceneComponent;
+class UBillboardComponent;
+class UDebugTextComponent;
+
+UENUM()
+enum class ERadiusRulerShape : uint8
+{
+	Circle,
+	Sphere,
+	Cylinder
+};
+
+UCLASS(Blueprintable, HideCategories=(Actor, HLOD, Replication, Input, Cooking, Collision, Physics, Rendering, WorldPartition, DataLayers, Networking))
+class TRICKYLEVELHELPERS_API ARadiusRuler : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ARadiusRuler();
+
+protected:
+	virtual bool ShouldTickIfViewportsOnly() const override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	UPROPERTY()
+	TObjectPtr<USceneComponent> Root = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UBillboardComponent> Billboard = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UDebugTextComponent> DebugText = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="RadiusRuler", meta=(ClampMin="0"))
+	float Radius = 100.f;
+
+	UPROPERTY(EditAnywhere, Category="RadiusRuler")
+	FColor Color = FColor::Magenta;
+
+	UPROPERTY(EditAnywhere, Category="RadiusRuler")
+	ERadiusRulerShape Shape = ERadiusRulerShape::Sphere;
+
+	UPROPERTY(EditAnywhere,
+		Category="RadiusRuler",
+		meta=(EditCondition="Shape==ERadiusRulerShape::Cylinder", EditConditionHides, ClampMin="0"))
+	float Height = 500.f;
+
+	UPROPERTY(EditAnywhere, Category="RadiusRuler", meta=(MultiLine))
+	FString NoteText = "Radius Ruler";
+
+	UPROPERTY(EditAnywhere, Category="RadiusRuler", meta=(ClampMin="0", ClampMax="360"))
+	float TextAngleOffset = 0.f;
+
+	UPROPERTY(EditAnywhere, Category="RadiusRuler", meta=(AllowPrivateAccess))
+	bool bShowInGame = false;
+};
