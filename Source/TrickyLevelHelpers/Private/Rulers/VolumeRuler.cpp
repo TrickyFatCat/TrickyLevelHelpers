c18637584563e2708367aa43a6ed711065867207
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-04-22 22:59:52 +0300

    Create VolumeRuler class

---
+++ Source/TrickyLevelHelpers/Private/Rulers/VolumeRuler.cpp
@@ -0,0 +1,92 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Rulers/VolumeRuler.h"
+
+#include "Components/BillboardComponent.h"
+
+
+AVolumeRuler::AVolumeRuler()
+{
+	bIsEditorOnlyActor = true;
+
+#if WITH_EDITORONLY_DATA
+	PrimaryActorTick.bCanEverTick = true;
+
+	Root = CreateEditorOnlyDefaultSubobject<USceneComponent>("Root");
+	SetRootComponent(Root);
+	
+	Billboard = CreateEditorOnlyDefaultSubobject<UBillboardComponent>("Billboard");
+	Billboard->SetupAttachment(GetRootComponent());
+	
+	struct FConstructorStatics
+	{
+		ConstructorHelpers::FObjectFinder<UTexture2D> SpriteTexture;
+		FName ID_Misc;
+		FText NAME_Misc;
+
+		FConstructorStatics()
+			: SpriteTexture(TEXT("/Engine/EditorResources/S_Note"))
+			  , ID_Misc(TEXT("Misc"))
+			  , NAME_Misc(NSLOCTEXT("SpriteCategory", "Misc", "Misc"))
+		{
+		}
+	};
+
+	static FConstructorStatics ConstructorStatics;
+	Billboard->SetSprite(ConstructorStatics.SpriteTexture.Object);
+	Billboard->bUseInEditorScaling = false;
+	Billboard->bIsScreenSizeScaled = true;
+	Billboard->ScreenSize = 0.001;
+
+#else
+	PrimaryActorTick.bCanEverTick = false;
+#endif
+}
+
+bool AVolumeRuler::ShouldTickIfViewportsOnly() const
+{
+#if WITH_EDITORONLY_DATA
+	return true;
+#else
+	return Super::ShouldTickIfViewportsOnly();
+#endif
+}
+
+void AVolumeRuler::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+#if WITH_EDITORONLY_DATA
+	Extent = Size * 0.5;
+	Center = GetActorLocation() + Extent;
+#endif
+}
+
+void AVolumeRuler::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+#if WITH_EDITORONLY_DATA
+
+	DrawDebugBox(GetWorld(),
+	             Center,
+	             Extent,
+	             Color,
+	             false,
+	             -1,
+	             0,
+	             Thickness);
+
+	if (bDrawSolidBox)
+	{
+		DrawDebugSolidBox(GetWorld(),
+		                  Center,
+		                  Extent - 0.1,
+		                  GetActorRotation().Quaternion(),
+		                  Color,
+		                  false,
+		                  -1,
+		                  0);
+	}
+#endif
+}
---
+++ Source/TrickyLevelHelpers/Public/Rulers/VolumeRuler.h
@@ -0,0 +1,52 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "VolumeRuler.generated.h"
+
+class USceneComponent;
+class UBillboardComponent;
+class UDebugTextComponent;
+
+UCLASS()
+class TRICKYLEVELHELPERS_API AVolumeRuler : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AVolumeRuler();
+
+private:
+	virtual bool ShouldTickIfViewportsOnly() const override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+	
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY()
+	TObjectPtr<USceneComponent> Root = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UBillboardComponent> Billboard = nullptr;
+
+	UPROPERTY()
+	TObjectPtr<UDebugTextComponent> DebugTextComponent = nullptr;
+
+	UPROPERTY(EditAnywhere, Category="VolumeRuler", meta=(AllowPreserveRatio))
+	FVector Size{100.f};
+
+	FVector Extent = Size * 0.5;
+
+	FVector Center{FVector::Zero()};
+
+	UPROPERTY(EditAnywhere, Category="VolumeRuler")
+	FColor Color{255, 0, 0, 50};
+
+	UPROPERTY(EditAnywhere, Category="VolumeRuler", meta=(ClampMin="1"))
+	float Thickness = 5.f;
+
+	UPROPERTY(EditAnywhere, Category="VolumeRuler")
+	bool bDrawSolidBox = false;
+};
